e7b175628551ba1ba67cba8fe6a5344e9ce0df6f
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2021-11-18 22:05:52 +0300

    Add readme and licence

---
+++ Readme.markdown
@@ -0,0 +1,7 @@
+# Göktuğ’s homepage
+
+This repository contains the public sources and scripts for [my
+website](https://gkayaalp.com).
+
+Everything here is licenced under the [CreativeCommons BY-NC-ND 4.0
+licence](./LICENCE_CC_BY_NC_ND_4.0.txt), unless noted otherwise.
--- transform.sh
+++ transform.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # transform.sh --- markdown -> html
 
+# Copyright (C) 2021 İ. Göktuğ Kayaalp <self at gkayaalp dot com> This
+# file is part of “Göktuğ’s homepage”.
+#
+# “Göktuğ’s homepage” is non-violent software: you can use,
+# redistribute, and/or modify it under the terms of the CNPLv6+ as
+# found in the LICENCE_CNPLv6.txt file in the source code root
+# directory or at <https://git.pixie.town/thufie/CNPL>.
+#
+# “Göktuğ’s Gemini Scripts” comes with ABSOLUTELY NO WARRANTY, to the
+# extent permitted by applicable law.  See the CNPL for details.
+
+
 # POSIX strict-ish mode, beware eager pipelines!
 set -eu
 
